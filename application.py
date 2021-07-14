b40cdd9850fced9ad02254eef7b8bf258bd463fd
Stu <saw.primesignups@outlook.com>
2021-07-14 15:21:16 +0100

    Commented out complex env variables to test

--- application.py
+++ application.py
@@ -6,9 +6,10 @@ from os import environ
 from blankFlaskApp import app
 
 if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '80'))
-    except ValueError:
-        PORT = 80
+    app.run(host='0.0.0.0', port=80)
+#    HOST = environ.get('SERVER_HOST', 'localhost')
+#    try:
+#        PORT = int(environ.get('SERVER_PORT', '80'))
+#    except ValueError:
+#       PORT = 80
     app.run(HOST, PORT)
